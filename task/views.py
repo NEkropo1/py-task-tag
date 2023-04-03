b80e7b12a84d1946ee5746dfabccd2eb4adcc56a
Paul Savchuk <nekrjkeee@gmail.com>
2023-04-03 11:59:02 +0300

    add urls for app task and basic skeleton for views and urls in task folder

--- task/views.py
+++ task/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views import generic
+
+from task.forms import TaskForm
+from task.models import Task, Tag
+
 
 # Create your views here.
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    queryset = Task.objects.select_related("tag")
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    queryset = Task.objects.select_related("tag")
+
+
+class TagListView(generic.ListView):
+    model = Tag
