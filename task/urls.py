636eddeca6843c3c711648f5cb9db50c0e10655b
Paul Savchuk <nekrjkeee@gmail.com>
2023-04-03 12:17:16 +0300

    refactor setting to right fields

--- task/urls.py
+++ task/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from task.views import TaskListView
+from task.views import TaskListView, TagListView, TaskCreateView
 
 urlpatterns = [
-    path("", TaskListView.as_view(), name="task-list"),
+    path("", TaskListView.as_view(), name="tasks"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path("tags/", TagListView.as_view(), name="tags"),
 ]
 
 app_name = "task"
